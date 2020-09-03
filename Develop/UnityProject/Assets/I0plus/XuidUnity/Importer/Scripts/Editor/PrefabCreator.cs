6196e4ce21055cb415bb51089b70d5694792cde7
Hiroki Ito <itouh2@i0plus.jp>
2020-09-03 10:50:39 +0900

    [modify] deactive unused objects in nested prefab.

--- Develop/UnityProject/Assets/I0plus/XuidUnity/Importer/Scripts/Editor/PrefabCreator.cs
+++ Develop/UnityProject/Assets/I0plus/XuidUnity/Importer/Scripts/Editor/PrefabCreator.cs
@@ -7,9 +7,9 @@ using MiniJSON;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
-
 #if TMP_PRESENT
 using TMPro;
+
 #endif
 
 namespace I0plus.XduiUnity.Importer.Editor
@@ -33,13 +33,14 @@ namespace I0plus.XduiUnity.Importer.Editor
             nestedPrefabs = prefabs;
         }
 
-        public void Create(ref GameObject targetObject, RenderContext renderContext, Dictionary<string, object> rootJson)
+        public void Create(ref GameObject targetObject, RenderContext renderContext,
+            Dictionary<string, object> rootJson)
         {
             if (EditorApplication.isPlaying) EditorApplication.isPlaying = false;
 
             var rootElement = ElementFactory.Generate(rootJson, null);
             rootElement.Render(ref targetObject, renderContext, null);
-            
+
             if (renderContext.ToggleGroupMap.Count > 0)
             {
                 // ToggleGroupが作成された場合
@@ -54,26 +55,44 @@ namespace I0plus.XduiUnity.Importer.Editor
 
             // 使われなかったオブジェクトの退避グループ
             var notUsedGroupObject =
-                ElementUtil.GetOrCreateGameObject(renderContext, null, "!XuidUnity Not Used", null);
+                ElementUtil.GetOrCreateGameObject(renderContext, null, $"[{Importer.NAME}] Unused", null);
             var notUsedchilds = renderContext.FreeChildObjects;
             if (notUsedchilds.Count > 0)
             {
-                notUsedGroupObject.SetActive(false);
-                var groupRect = ElementUtil.GetOrAddComponent<RectTransform>(notUsedGroupObject);
-                groupRect.SetParent(targetObject.transform);
+                var notUsedGroupRect = ElementUtil.GetOrAddComponent<RectTransform>(notUsedGroupObject);
                 foreach (var keyValuePair in notUsedchilds)
                 {
                     var go = keyValuePair.Key;
                     if (go != null)
                     {
-                        var goRect = ElementUtil.GetOrAddComponent<RectTransform>(go);
-                        // NestedPrefabの子供を転送しようとするとエラーになる
-                        goRect.SetParent(groupRect);
+                        if (PrefabUtility.GetNearestPrefabInstanceRoot(go) == null)
+                        {
+                            // NestedPrefabの子供でなければNotUsedグループに移動できる
+                            var goRect = go.GetComponent<RectTransform>();
+                            if (goRect != null)
+                            {
+                                goRect.SetParent(notUsedGroupRect);
+                            }
+                        }
+                        else
+                        {
+                            // NestedPrefabの子供を転送しようとするとエラーになるため、
+                            // 非アクティブにする
+                            //TODO: unusedといった名前をつけるか検討
+                            go.SetActive(false);
+                        }
                     }
                     else
                     {
                         // Debug.Log("既に廃棄されています");
                     }
+                }
+
+                // not used グループに子供があった場合のみ移動
+                if (notUsedGroupRect.childCount > 0)
+                {
+                    notUsedGroupRect.SetParent(targetObject.transform);
+                    notUsedGroupObject.SetActive(false);
                 }
             }
             else
